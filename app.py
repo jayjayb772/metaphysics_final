f54cc1896256b0cc007ce65468141516805cb82d
jayjayb772 <jayjayb100@msn.com>
2019-10-29 15:09:30 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, redirect, url_for, request, jsonify, json
+
+app = Flask(__name__)
+
+@app.route('/')
+def mainPage():
+   return "Hello world!"
+
+@app.route('/changeColor', methods=['POST'])
+def changeColor():
+   data = request.get_json(force=True)
+   red=data['red']
+   blue=data['blue']
+   green=data['green']
+
+
+if __name__ == '__main__':
+   app.run(host=0.0.0.0)
